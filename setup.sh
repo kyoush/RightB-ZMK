a5f69c4d8ad9deb1df2791107dfba0976f9d0021
Kyoshiro Kamura <32662441+kyoush@users.noreply.github.com>
2026-08-10 08:16:32 +0900

    :hammer: build: manage ZMK modules with ZMK CLI

--- setup.sh
+++ setup.sh
@@ -4,38 +4,14 @@ set -euo pipefail
 USER_UID="$(id -u)"
 USER_GID="$(id -g)"
 
-if ! docker compose run --rm zmk test -d /workspaces/zmk/.west; then
-    echo "==> Creating ZMK workspace..."
-
-    docker compose run --rm \
-        --user root \
-        zmk \
-        sh -c '
-set -e
-
-cd /workspaces
-
-git clone https://github.com/zmkfirmware/zmk zmk
-
-cd zmk
-west init -l app/
-west update
-'
-fi
-
-docker compose run --rm \
-    --user root \
-    zmk \
-    chown -R "${USER_UID}:${USER_GID}" /workspaces/zmk
-
 docker compose run --rm \
     --user "${USER_UID}:${USER_GID}" \
     zmk \
     sh -c '
 set -e
 
-cd /workspaces/zmk
+cd /workspaces/zmk-config
 
-echo "==> Updating workspace..."
-west update
+zmk config user.home /workspaces/zmk-config
+zmk update
 '
