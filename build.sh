82f451d90c7a2e0e782db964a9fb4bc757c49df3
Kyoshiro Kamura <32662441+kyoush@users.noreply.github.com>
2026-08-10 07:57:21 +0900

    :hammer: build: run ZMK build in container

--- build.sh
+++ build.sh
@@ -1,33 +1,16 @@
 #!/usr/bin/env bash
-set -eu
+set -euo pipefail
 
-WORKSPACE="$HOME/src/zmk"
-
-CONTAINER_ID="$(
-  devcontainer up \
-    --workspace-folder "$WORKSPACE" \
-    --log-format json \
-    2>/dev/null \
-  | jq -r 'select(.containerId) | .containerId'
-)"
-
-if [ -z "$CONTAINER_ID" ]; then
-  echo "ZMK Dev Container is not running." >&2
-  exit 1
-fi
-
-docker exec -it "$CONTAINER_ID" bash -lc "
-cd /workspaces/zmk
-
-# Enable USB logging (CDC ACM) for development.
-west build \
-    \$@ \
-  -s app \
-  -d build \
-  -b xiao_ble/nrf52840/zmk \
-  -S zmk-usb-logging \
-  -- \
-  -DZMK_CONFIG=/workspaces/zmk-config/config \
-  -DZMK_EXTRA_MODULES=/workspaces/zmk-config \
-  -DSHIELD=rightb
-" -- "$@"
+docker compose run --rm \
+    --user "$(id -u):$(id -g)" \
+    zmk \
+    west build \
+        "$@" \
+        -s app \
+        -d build \
+        -b xiao_ble/nrf52840/zmk \
+        -S studio-rpc-usb-uart \
+        -- \
+        -DZMK_CONFIG=/workspaces/zmk-config/config \
+        -DZMK_EXTRA_MODULES=/workspaces/zmk-config \
+        -DSHIELD=rightb
